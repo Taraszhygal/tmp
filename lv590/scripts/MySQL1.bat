828d6b5eb596310aa915b1826ddf7e7df78c1740
Taraszhygal <zhygalt@gmail.com>
2021-07-27 01:35:24 +0300

    upload commit

---
+++ lv590/scripts/MySQL1.bat
@@ -0,0 +1,7 @@
+rem Goto to C:\Program Files\MySQL\MySQL Server 5.7\bin
+rem mysql -p -u root lv459oms < MySQL.sql
+
+rem mysql -p -u root lv459oms < MySQL.sql
+
+mysql -h 192.168.153.134 -u devops --password="DevOps_553" < MySQL.sql
+
---
+++ lv590/scripts/MySQL1.sh
@@ -0,0 +1,1 @@
+mysql -h 192.168.153.131 -u devops --password="DevOps_553" < MySQL.sql 
